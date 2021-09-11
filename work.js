5d0339b22422f7fcd2b777800f3a42aae7f56114
Joshman <ejembijoshman@gmail.com>
2021-09-11 14:09:38 +0100

    first commit

---
+++ work.js
@@ -0,0 +1,47 @@
+// function meFunct(){
+//     document.body.style.backgroundImage="background-123.jpg";
+// }
+
+// "document.body.style.backgroundColor='red'";
+
+// function myFuct(){
+//     document.body.style.backgroundImage = "url('background-123.jpg')";
+// }
+
+
+// const hamburger = document.querySelector(".hamburger");
+// const navLinks = document.querySelector(".nav-links");
+
+// hamburger animation for open and close X
+// hamburger.addEventListener("click" , mobileMenu);
+
+// function mobileMenu() {
+//     hamburger.classList.toggle("active");
+//     navLinks.classList.toggle("active");
+// }
+
+// function to make hamburger open and close nav-bar menu
+function menuToggle() {
+    var nav = document.getElementById('myNav');
+    nav.classList.toggle('active');
+    var nav = document.getElementById('toggleIcon');
+    nav.classList.toggle('active');
+}
+
+
+// function openNav() {
+//     document.getElementById("myLink").style.display = "show";
+// }
+
+// function closeNav() {
+//     document.getElementById("myLink").style.display = "none";
+// }
+
+// const navItem = document.querySelectorAll(".nav-item");
+
+// navItem.forEach(n => n.addEventListener("click" . closeMenu));
+
+// function closeMenu() {
+//     hamburger.classList.remove("active");
+//     navLinks.classList.remove("active");
+// }
